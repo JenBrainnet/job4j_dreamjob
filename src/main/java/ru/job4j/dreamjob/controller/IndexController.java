9c7cf6562afd572e0477e53d1814b2ac181917ef
JenBrainnet <benebrak01@gmail.com>
2026-04-14 11:14:03 +0300

    Implemented "Hello, World!"

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,13 +1,14 @@
 package ru.job4j.dreamjob.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@SuppressWarnings("unused")
+@RestController
 public class IndexController {
+
     @GetMapping("/index")
     public String getIndex() {
-        return "index";
+        return "Hello, World!";
     }
+
 }
